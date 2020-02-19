a9f699810796731b1ae8f1aeed3b4e9cfeea2fd6
Brian Wei <bwei98@protonmail.com>
2020-02-18 19:50:34 -0500

    Add error checking

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * main.c: This program allocates some memory then frees it.
  *
  *
- * Author: Jeremy Dropkin
+ * Author: Jeremy Dropkin, Brian Wei
  * Date: Feb 18 2020
  */
 
@@ -10,6 +10,9 @@
 
 int main() {
   void *a = malloc(127361238);
+  if (a == NULL) {
+    return 1;
+  }
   free(a);
   return 0;
 }
